a09eb5f76bf8e3370f37fa0a9230dbad7e798ebd
Marcelo Soares <mamsoares@gmail.com>
2021-05-04 20:40:19 -0300

    Change css in footer and add new styles

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
         <p className="text-light">App desenvolvido por <a href="https://github.com/mamsoares" target="_blank" rel="noreferrer">Marcelo Soares</a></p>
+        <p className="text-light">LinkedIn <a href="https://linkedin.com/in/mamsoares" target="_blank" rel="noreferrer">Marcelo Soares</a></p>
         <p className="text-light"><small><strong>Semana Spring React</strong><br/>
           Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
