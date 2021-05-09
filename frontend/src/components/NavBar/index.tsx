d47b8c40b4e80b07f19f4e6dc96aeaf8aea79fd6
Marcelo Soares <mamsoares@gmail.com>
2021-05-09 09:40:27 -0300

    feat: change logo and include background color in Header compenent

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,10 @@
-import ImgDsDark from "assets/img/ds_dark.svg"
+import ImgDsDark from "assets/img/complete_logo_dvsuperior.svg"
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm bg-dark">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
           <Link to="/">
